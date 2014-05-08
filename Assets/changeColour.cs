bab64ba96f6cfc623688231d9bd2ddf5bebca06d
mikdsouza <mikdsouza34@gmail.com>
2014-05-08 17:01:40 +1200

    Added a sphere behind the fiducial marker with a plane to indicate its location

---
+++ Assets/changeColour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeColour : MonoBehaviour {
+	private int num;
+	void start(){
+		renderer.material.color = Color.green;
+		num = 1;
+	}
+
+	void OnMouseUp(){
+		
+			if(num == 1) {
+				renderer.material.color = Color.red;
+				num = 0;
+			}
+			else {
+				renderer.material.color = Color.green;
+				num = 1;
+			}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
