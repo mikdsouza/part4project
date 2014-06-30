1b232aaf24ed15c99a0235c0570d61aa2b738d04
mikdsouza <mikdsouza34@gmail.com>
2014-06-30 16:26:40 +1200

    Added more GUI skins and re-skined the main menu

--- Assets/MainMenu/Scripts/MainMenu.cs
+++ Assets/MainMenu/Scripts/MainMenu.cs
@@ -2,18 +2,27 @@
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
-
+	public GUISkin MainMenuGUISkin;
+		
 	void OnGUI () {
+		MainMenuGUISkin.button.fontSize = (int) heightPercentage(7);
+		MainMenuGUISkin.box.fontSize = (int) heightPercentage(9);
+		GUI.skin = MainMenuGUISkin;
+		createMenu();
+	}
+	
+	//Crete the main menu
+	void createMenu() {
 		// Make a background box
 		GUI.Box(new Rect(widthPercentage(10),heightPercentage(10),widthPercentage(80),heightPercentage(80)), "Main Menu");
 		
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(widthPercentage(20),heightPercentage(20),widthPercentage(60),heightPercentage(10)), "Something button")) {
+		if(GUI.Button(new Rect(widthPercentage(20),heightPercentage(24),widthPercentage(60),heightPercentage(10)), "Scenes")) {
 			Application.LoadLevel("example-scene");
 		}
 		
 		// Make the second button.
-		if(GUI.Button(new Rect(widthPercentage(20),heightPercentage(40),widthPercentage(60),heightPercentage(10)), "Exit")) {
+		if(GUI.Button(new Rect(widthPercentage(20),heightPercentage(36),widthPercentage(60),heightPercentage(10)), "Exit")) {
 			Quit();
 		}
 	}
