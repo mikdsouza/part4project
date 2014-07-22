7e1a416cffd25d2c4974f9693cda1a07ebae79e7
mikdsouza <mikdsouza34@gmail.com>
2014-07-22 16:34:28 +1200

    Added a script to apply all scripts to a scene

--- Assets/MainMenu/Scripts/SceneSelect.cs
+++ Assets/MainMenu/Scripts/SceneSelect.cs
@@ -37,6 +37,7 @@ public class SceneSelect : MonoBehaviour {
 		levels.Add("ShowOff-Scene");
 		levels.Add ("HourGlass-Scene");
 		levels.Add ("MultiMarker-Scene");
+		levels.Add ("AutoApplyTest-Scene");
 //		levels.Add("lol2");
 //		levels.Add("lol3");
 //		levels.Add("lol4");
---
+++ Assets/Scenes/Common/ApplyAll.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ApplyAll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {		
+		//Apply the back to main menu behaviour
+		gameObject.AddComponent("BackToMainMenu");
+		
+		//Apply the Object counting scripts
+		List<DefaultTrackableEventHandler> markers = new List<DefaultTrackableEventHandler> ();
+
+		GameObject[] objs = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
+		foreach (GameObject obj in objs) {
+			if(obj.GetComponent<DefaultTrackableEventHandler>() != null) {
+				gameObject.AddComponent("ObjectCountEventHandler");
+			}
+		}
+		gameObject.AddComponent("ObjectCountOffset");
+		
+		//Apply save button
+		gameObject.AddComponent("SaveButton");
+		SaveButton sb = gameObject.GetComponent(typeof(SaveButton)) as SaveButton;
+		sb.style = new GUIStyle();
+		sb.imageSave = Resources.Load<Texture2D>("save-icon");
+		sb.imageLoad = Resources.Load<Texture2D>("open-icon");
+		
+		//Apply the screenshot feature
+		gameObject.AddComponent("ScreenshotButton");
+		ScreenshotButton sc = gameObject.GetComponent(typeof(ScreenshotButton)) as ScreenshotButton;
+		sc.image = Resources.Load<Texture2D>("screenshot-icon");
+		sc.style = new GUIStyle();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
