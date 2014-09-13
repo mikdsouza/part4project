a4553f33263cabc42cc3f2a92c06ee01a19af7c5
Mikhail D'Souza <mikdsouza34@gmail.com>
2014-09-13 22:31:39 +1200

    Made show off scene even betterer

--- Assets/Scenes/ShowOffScene/jetScript.cs
+++ Assets/Scenes/ShowOffScene/jetScript.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class jetScript : MonoBehaviour {
+	public Rigidbody missile1, missile2;
+	private bool missile2Fire = false;
 
 	// Use this for initialization
 	void Start () {
@@ -15,5 +17,23 @@ public class jetScript : MonoBehaviour {
 
 	Vector3 GetCurrentOffset () {
 		return new Vector3(0, 0, (float) (0.002 * System.Math.Sin(Time.time * 2)));
+	}
+
+	void OnMouseUp() {
+		Rigidbody clone, missile;
+
+		if (missile2Fire)
+			missile = missile1;
+		else
+			missile = missile2;
+
+		missile2Fire = !missile2Fire;
+
+		clone = Instantiate(missile, missile.GetComponent<Transform>().position + (new Vector3(0,(float)-0.1,0))
+			, missile.GetComponent<Transform>().rotation) as Rigidbody;
+		clone.AddRelativeForce(new Vector3(100,0,0));
+		clone.GetComponentInChildren<ParticleEmitter>().emit = true;
+		clone.GetComponent<Transform>().localScale = new Vector3((float)0.04,(float)0.04,(float)0.04);
+		Destroy(clone.gameObject, 1);
 	}
 }
