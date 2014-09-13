5194885ba35135b3e723c52f4392a7913adb1ab8
Mikhail D'Souza <mikdsouza34@gmail.com>
2014-09-13 20:27:34 +1200

    New Show-Off Scene

---
+++ Assets/Scenes/ShowOffScene/jetScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jetScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(GetCurrentOffset());
+	}
+
+	Vector3 GetCurrentOffset () {
+		return new Vector3(0, 0, (float) (0.002 * System.Math.Sin(Time.time * 2)));
+	}
+}
