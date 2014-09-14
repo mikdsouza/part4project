998deea125552457ef84812b87cd481672e5dabc
Mikhail D'Souza <mikdsouza34@gmail.com>
2014-09-14 14:43:32 +1200

    Added a fake shadow below the plane

---
+++ Assets/Scenes/ShowOffScene/shadowScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shadowScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale += GetCurrentOffset();
+	}
+
+	Vector3 GetCurrentOffset () {
+		return new Vector3((float) (-0.0002 * System.Math.Sin(Time.time * 2)), 
+		                   (float) (-0.0002 * System.Math.Sin(Time.time * 2)), 
+		                   (float) (-0.0002 * System.Math.Sin(Time.time * 2)));
+	}
+}
